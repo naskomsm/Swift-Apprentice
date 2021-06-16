635f67190aed067c62268667ad42ad11e62ce6d0
Atanas Kolev <atanaskolev@Atanass-Mac-mini.local>
2021-06-16 17:39:30 +0300

    Chapter 2

---
+++ Chapter2.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+// Challange 1
+let coordinates: (Int,Int) = (2,3)
+
+// Challange 2
+let namedCoordinate: (row: Int, col: Int) = (2,3)
+namedCoordinate.row
+
+// Challange 3
+let someString: String = "Dog"
+let someCharacter: Character = "🐶"
+ 
+// Challange 4
+let tuple = (day:15, month: 8, year: 2015)
+let day = tuple.day
+
+// Challange 5
+var name = "Matt"
+name += " Galloway"
+
+// Challange 6
+let tuple2: (Int, Double, Int) = (100, 1.5, 10)
+let value: Double = tuple2.1
+
+// Challange 7
+let tuple3: (day: Int, month: Int, year: Int) = (day: 15, month:8, year: 2015)
+let month = tuple3.month
+
+// Challange 8
+let number = 10
+let multipier = 5
+let summary = "\(number) multipled by \(multipier) equals \(number * multipier)"
+
+// Challange 9
+let a = 4
+let b: Int32 = 100
+let c: UInt8 = 12
