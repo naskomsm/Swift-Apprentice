3d9952f9ee5cd94d2800ae01bb3f0b718cc6c3a9
Atanas Kolev <atanaskolev@Atanas-Mac-Mini.local>
2021-07-07 13:54:34 +0300

    Chapter 3 Finished

--- Chapter3.playground/Contents.swift
+++ Chapter3.playground/Contents.swift
@@ -49,5 +49,63 @@ let number = 3.0
 let power = number + 2.0
 let result = pow(number, power)
 
-// Triangular number
+// Challenge 6 - Reverse Triangular number
+let givenNumber = 15.0
+let tResult = floor(sqrt(givenNumber * 2))
 
+// Challenge 7
+func fibonacci(_ n: Int) -> Int {
+    if(n == 0) {
+        return 0
+    } else if (n == 1){
+        return 1
+    }
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+}
+let fibResult = fibonacci(4)
+
+// Challange 8
+for n in 1...12 {
+    print(pow(Decimal(n), 2))
+}
+
+// Challange 9
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
+func printAllSumRecords(target: Int, currentSum: Int, start: Int, result: inout [Int], output: inout [Int]) {
+    if(currentSum == target){
+        if(result.count == 2){
+            output.append(contentsOf: result)
+        }
+    }
+    
+    for n in start...target{
+        let tempSum = currentSum + n
+        if(tempSum <= target && tempSum <= 12) {
+            result.append(n)
+            printAllSumRecords(target: target, currentSum: tempSum, start: n, result: &result, output: &output)
+            result.removeLast()
+        } else
+        {
+            return
+        }
+    }
+}
+
+func printAllSum(target: Int) -> [Int] {
+    var output: [Int] = []
+    var result: [Int] = []
+    printAllSumRecords(target: target, currentSum: 0, start: 1, result: &result, output: &output);
+    return output
+}
+
+for n in 2...12 {
+    print(printAllSum(target: n).chunked(into: 2))
+}
